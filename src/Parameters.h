40b12280fce64168e8f16c247e1a6d96660af63e
Derek Everett <derek@dereks-mbp.asc.ohio-state.edu>
2017-10-05 18:41:48 -0400

    updated plotting notebook

--- src/Parameters.h
+++ src/Parameters.h
@@ -1,7 +1,7 @@
 #define PI 3.141592654f //question bro?
 #define SIGMA 1.0f //the width of the gaussian distribution in (y - eta). Generalize this to a spacetime function?
-#define DIM_X 41 //number of grid points in x direction
-#define DIM_Y 41 //number of grid points in y direction
+#define DIM_X 101 //number of grid points in x direction
+#define DIM_Y 101 //number of grid points in y direction
 #define DIM_ETA 41 //number of grid points in eta (spacetime rapidity) direction
 #define DIM (DIM_X * DIM_Y * DIM_ETA) //total number of spatial grid points
 #define DIM_RAP 31 //number of grid points in momentum rapidity
@@ -11,6 +11,6 @@
 #define DETA 0.05f //spacing of grid in eta direction
 #define DRAP 0.1f //spacing of grid in momentum rapidity
 #define DTAU 0.1f //free streaming longitudinal proper time step size
-#define TAU0 0.1f //initial long. proper time
+#define TAU0 0.1f //initial longitudinal proper time
 #define TAU (TAU0 + DTAU) //final matching longitudinal proper time
 #define EOS_TYPE 2 // 1 for conformal EOS, 2 for Wuppertal-Budhapest parameterization
