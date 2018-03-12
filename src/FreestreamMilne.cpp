94e718f6baaae84a3f833e2dbc7dfdc070684dfd
Derek Everett <derekeverett@gmail.com>
2018-03-12 15:27:59 -0400

    Changed HBARC to be defined as a double rather than a float

--- src/FreestreamMilne.cpp
+++ src/FreestreamMilne.cpp
@@ -22,7 +22,8 @@
 
 #define PI 3.141592654f
 #define PRINT_SCREEN 1 //turn on for program info to print to terminal
-#define HBARC 0.197326938f
+//#define HBARC 0.197326938f
+#define HBARC 0.197326938 //used to convert units of input / output hydro vectors 
 
 using namespace std;
 
